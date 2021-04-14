f59b066a6e405ea91f6a4bb9953873c0941adf4b
Sanaa Khelloqi <sanaa-khelloqi@hotmail.fr>
2021-04-14 10:55:34 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pcapstats",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     license="Apache License 2.0",
     long_description=open("README.md").read(),
